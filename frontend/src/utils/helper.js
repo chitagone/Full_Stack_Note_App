1711e918b4c5e13e87693f59177feca264d4b10a
Chittagone <chitagobego@gmail.com>
2024-08-05 12:01:22 +0700

    add button and fish navbar and card

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -8,3 +8,15 @@ export const validateEmail = (email) => {
 export const validatePassword = (password) => {
   return password.length >= 6;
 };
+
+// algorithm for split the name
+export const getInitials = (name) => {
+  if (!name) return "";
+
+  const words = name.split(" ");
+  let initials = "";
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
+    initials += words[i][0];
+  }
+  return initials.toUpperCase();
+};
